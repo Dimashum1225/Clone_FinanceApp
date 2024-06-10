0791124d2c85b8310053929c099bcba04d90a5d9
Dima Shumatbaev <sumatbaev7@gmail.com>
2024-06-10 19:13:43 +0500

    добавил календарь для выбора

--- app/src/main/java/com/example/clone_financeapp/AddIncomesActivity.kt
+++ app/src/main/java/com/example/clone_financeapp/AddIncomesActivity.kt
@@ -1,11 +1,41 @@
 package com.example.clone_financeapp
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.util.Calendar
 
 class AddIncomesActivity : AppCompatActivity() {
+
+    lateinit var pickDateBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_incomes)
+
+        pickDateBtn = findViewById(R.id.choose_date)
+
+        pickDateBtn.setOnClickListener{
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(
+                this,
+                { view, year, monthOfYear, dayOfMonth ->
+                    // on below line we are setting
+                    // date to our text view.
+                    pickDateBtn.text =
+                        (dayOfMonth.toString() + "_" + (monthOfYear + 1) + "_" + year)
+                },
+                // on below line we are passing year, month
+                // and day for the selected date in our date picker.
+                year,
+                month,
+                day
+            )
+            datePickerDialog.show()
+        }
     }
 }
